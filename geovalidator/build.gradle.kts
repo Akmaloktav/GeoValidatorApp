9b3301539a3716b39fcfe9744c1eccb58a6b19be
Akmaloktav <akmaloktavian139@gmail.com>
2025-07-03 05:24:32 +0700

    Fix: Configure JitPack build and publishing

--- geovalidator/build.gradle.kts
+++ geovalidator/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Akmaloktav"
             artifactId = "geovalidator"
-            version = "1.0.0"
+            version = "1.0.1"
             afterEvaluate {
                 from(components["release"])
             }
